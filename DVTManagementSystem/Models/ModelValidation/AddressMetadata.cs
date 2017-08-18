6a2828c81fb35f79333b18dd747aa7ab62bc476f
Vivian <vivian.tebeila@gmail.com>
2017-08-18 08:10:18 +0200

    Models and Migrations

---
+++ DVTManagementSystem/Models/ModelValidation/AddressMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DVTManagementSystem.Models.ModelValidation
+{
+    public class AddressMetadata
+    {
+
+        [Required]
+        public int AddressTypeId { get; set; }
+
+        [Required ]
+        public int SuburbId { get; set; }
+        
+        [ForeignKey("AddressTypeId")]
+        public virtual AddressType AddressType { get; set; }
+
+        [ForeignKey("SuburbId")]
+        public virtual Suburb Suburb { get; set; }
+    }
+}
---
+++ DVTManagementSystem/Models/ModelValidation/UserTypeMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DVTManagementSystem.Models.ModelValidation
+{
+    public class UserTypeMetadata
+    {
+        [Required ]
+        [StringLength (100)]
+        public string UserRole { get; set; }
+    }
+}
