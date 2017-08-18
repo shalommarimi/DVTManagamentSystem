028afcfcefda7cc58d23b2643081bac68703056e
shalommarimi <shalommarimi@gmail.com>
2017-08-18 08:32:41 +0200

    Created Autherization Controller

---
+++ DVTManagementSystem/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DVTManagementSystem.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
