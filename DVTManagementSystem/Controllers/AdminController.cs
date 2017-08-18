e9004cf35dae92f29b15655c0bf3e49239626e32
Sindi Ludmiller <lsmashinini@gmail.com>
2017-08-18 11:48:13 +0200

    Created New Controller

---
+++ DVTManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DVTManagementSystem.Models;
+using DVTManagementSystem.Migrations;
+
+namespace DVTManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult ListOfUser()
+        {
+
+            return View();
+        }
+    }
+}
