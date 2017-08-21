a7ad53829946d6a7e742deb5285bd5ab11410e98
munwininokuthulazwane <nokuthulazwane40@gmail.com>
2017-08-21 10:40:51 +0200

    reset password model

---
+++ DVTManagementSystem/Controllers/Account/ResetPasswordController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using DVTManagementSystem.Models.AccountModel;
+
+
+namespace DVTManagementSystem.Controllers.Account
+{
+    public class ResetPasswordController : Controller
+    {
+        // GET: ResetPassword
+        public ActionResult ResetPassword( string resettoken)
+        {
+            ResetPasswordModel _resetPasswordmodel = new ResetPasswordModel();
+            _resetPasswordmodel.ReturnToken = resettoken;
+
+            return View(_resetPasswordmodel);
+        }
+    }
+}
---
+++ DVTManagementSystem/Models/AccountModel/ResetPasswordModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using DVTManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DVTManagementSystem.Models.AccountModel
+{
+    public class ResetPasswordModel
+    {
+         [Required]
+         [Display(Name ="New Password")]
+         [DataType(DataType.Password)]
+         public string Password { get; set; }
+
+        [Required]
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)] 
+        [Compare("Password",ErrorMessage ="Password does not match with the new Password")] 
+         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ReturnToken { get; set; }
+    }
+}
