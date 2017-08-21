3a61242d36631b1028c06eae9508319548dd3431
Sindi Ludmiller <lsmashinini@gmail.com>
2017-08-21 13:24:14 +0200

    Update User method

--- DVTManagementSystem/Controllers/AdminController.cs
+++ DVTManagementSystem/Controllers/AdminController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using DVTManagementSystem.Models;
 using DVTManagementSystem.Migrations;
 using DVTManagementSystem.Models.Context;
+using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace DVTManagementSystem.Controllers
 {
@@ -15,8 +17,42 @@ namespace DVTManagementSystem.Controllers
         // GET: Admin
         public ActionResult UserList()
         {
-            var users= DVTcontext.UserProfiles.ToList();
+            var users = DVTcontext.UserProfiles.ToList();
             return View(users);
         }
+        // GET: Users/Approved 
+        public ActionResult ApprovedUser(int id)
+        {
+            var UserUpdate = DVTcontext.UserProfiles.Where(u => u.UserProfileId == id).FirstOrDefault();
+            return View(UserUpdate);
+        }
+
+        // POST: Approving users / editing user status 
+        
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ApprovedUser([Bind(Include = "UserId,FirstName,LastName,Status")] UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //update feilds 
+                user.FirstName = user.FirstName;
+                user.LastName = user.LastName;
+
+
+                DVTcontext.Entry(user).State = EntityState.Modified;
+
+                DVTcontext.SaveChanges();
+
+                return RedirectToAction("UserList");
+            }
+            return View(user);
+        }
+
     }
+
 }
+
+
