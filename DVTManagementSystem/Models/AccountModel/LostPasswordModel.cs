ac0e8b5f4f08ae01938c54df873188eb243c259d
munwininokuthulazwane <nokuthulazwane40@gmail.com>
2017-08-21 11:56:10 +0200

    Fixed the lostPaswordmodel

--- DVTManagementSystem/Models/AccountModel/LostPasswordModel.cs
+++ DVTManagementSystem/Models/AccountModel/LostPasswordModel.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Data.SqlClient;
 using DVTManagementSystem.Models;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 using WebMatrix.WebData;
 
 namespace DVTManagementSystem.Models.AccountModel
@@ -12,6 +13,9 @@ namespace DVTManagementSystem.Models.AccountModel
     public class LostPasswordModel
     {
         //waiting for the login model sothat i can envoke the model state
+        [Required(ErrorMessage ="Please enter your email address to receive the reset password link")]
+        [Display(Name ="Your email address")]
+        [EmailAddress(ErrorMessage ="Not a valid email address")]
         public string email { get; set; }
     }
 }
