8f5336c4be80bacb6ea98b9ecc22533d87cca00d
shalommarimi <shalommarimi@gmail.com>
2017-08-25 08:36:49 +0200

    Merging with Master

--- DVTManagementSystem/Controllers/AuthenticationController.cs
+++ DVTManagementSystem/Controllers/AuthenticationController.cs
@@ -27,18 +27,18 @@ namespace DVTManagementSystem.Controllers
                     var PasswordHashingmMethod = new PasswordHashing();
                     string HashedPassword = PasswordHashingmMethod.HashInput(profile.PasswordHash);
 
-                       
+
 
                     var user = context.UserProfiles.Single(u => u.EmailAddress == profile.EmailAddress && u.PasswordHash == profile.PasswordHash && u.IsApproved == true);
                     if (user != null)
                     {
-                        
+
                         Session["FirstName"] = user.FirstName.ToString();
                         Session["LastName"] = user.FirstName.ToString();
                         return RedirectToAction("Dashboard", "Applicant");
 
-                    } 
-                 
+                    }
+
                 }
                 catch (System.Exception)
                 {
