55ce64784f33cf290280281e6abc11d885a69b26
Lepelle Seshiba <LSeshiba@jhb.dvt.co.za>
2017-09-05 11:16:08 +0200

    added the reset password

--- DVTManagementSystem/Migrations/Configuration.cs
+++ DVTManagementSystem/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace DVTManagementSystem.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DVTManagementSystem.Models.Context.DVTManagementSystemContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.Context.DVTManagementSystemContext>
     {
         public Configuration()
         {
